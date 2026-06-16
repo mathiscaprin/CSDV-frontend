c6abd0d1d45a5fcf8ae9387186d9e92595b71873
Sylvain110 <sylvainmabit.101101@gmail.com>
2026-06-16 16:34:07 +0200

    add ux/ui changes

--- src/data/ranks.js
+++ src/data/ranks.js
@@ -1,8 +1,8 @@
 export const RANKS = [
   { name: '🎓 Stagiaire', threshold: 0 },
-  { name: '💼 Alternant', threshold: 1000 },
-  { name: '🧑‍💻 Junior', threshold: 10000 },
-  { name: '👨‍💼 Senior', threshold: 50000 },
-  { name: '🎤 Intervenant', threshold: 200000 },
+  { name: '💼 Alternant', threshold: 5000 },
+  { name: '🧑‍💻 Junior', threshold: 20000 },
+  { name: '👨‍💼 Senior', threshold: 100000 },
+  { name: '🎤 Intervenant', threshold: 500000 },
   { name: '🏛️ PDG', threshold: 1000000 },
 ]
